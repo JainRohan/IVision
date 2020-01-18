efdbbe46ca6d691efbd1f781b00c1513a8c6d5d9
kanav singla <47704494+kanav532@users.noreply.github.com>
2020-01-18 17:46:52 -0500

    Create images_loop.py

---
+++ video_detection/images_loop.py
@@ -0,0 +1,39 @@
+import time
+import cv2
+from imageai.Detection import ObjectDetection
+import os
+
+cam = cv2.VideoCapture(1)
+cv2.namedWindow("test")
+img_counter = 0
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
+detector.loadModel()
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("test", frame)
+    if not ret:
+        break
+
+    img_name = "frame_{}.png".format(img_counter)
+    cv2.imwrite(img_name, frame)
+    print("{} written!".format(img_name))
+    image_path = detector.detectObjectsFromImage(input_image=img_name, output_image_path=img_name)
+    print(image_path)  # gives probabilities
+
+    img_counter += 1
+
+    if cv2.waitKey(1) % 256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+
+    time.sleep(0.25)
+
+cam.release()
+
+cv2.destroyAllWindows()
