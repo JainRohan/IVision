e5f14988e8abac961e7e5526a218a4f2e3f26c93
RohanJain <rohan.jain@mail.utoronto.ca>
2020-01-18 16:03:10 -0500

    test2

---
+++ video_detection/firstcameradetection.py
@@ -0,0 +1,17 @@
+from imageai.Detection import VideoObjectDetection
+import os
+import cv2
+
+execution_path = os.getcwd()
+
+camera = cv2.VideoCapture(0)
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(os.path.join(execution_path , "yolo.h5"))
+detector.loadModel()
+
+video_path = detector.detectObjectsFromVideo(camera_input=camera,
+                                output_file_path=os.path.join(execution_path, "camera_detected_1")
+                                , frames_per_second=10, log_progress=True)
+print(video_path)
